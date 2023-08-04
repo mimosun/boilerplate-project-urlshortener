bda0164f0eaa83e8ad187ef3ca7083988edc2370
Replit user <>
2023-08-04 08:11:15 +0000

    URL shortener

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const session = require('express-session');
 const cors = require('cors');
 const app = express();
 
@@ -7,6 +8,14 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: 'secret',
+  cookie: { maxAge: 60000 }
+}));
+app.use(express.json()) // for json
+app.use(express.urlencoded({ extended: true })) // for form data
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -17,6 +26,44 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.get('/api/shorturl/:id', function(req, res) {
+  if (!req.params.id || isNaN(req.params.id)) {
+    res.json({ error: 'Wrong format' });
+    return
+  }
+  
+  if (!req.session.shortener || !req.session.shortener.hasOwnProperty(req.params.id)) {
+    res.json({ error: 'No short URL found for the given input' });
+    return
+  }
+
+  res.redirect(req.session.shortener[req.params.id]);
+});
+
+app.post('/api/shorturl', function(req, res) {
+  try {
+    new URL(req.body.url);
+  } catch (e) {
+    res.json({ error: 'invalid url' });
+    return
+  }
+
+  let shortUrl = 1;
+
+  if (!req.session.shortener) {
+    req.session.shortener = {};
+  } else {
+    shortUrl = parseInt(Object.keys(req.session.shortener).slice(-1)) + 1;
+  }
+
+  req.session.shortener[shortUrl] = req.body.url;
+
+  res.json({
+    original_url: req.body.url,
+    short_url: shortUrl
+  });
 });
 
 app.listen(port, function() {
